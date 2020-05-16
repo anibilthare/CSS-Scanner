16fb4a05a789b4360327de912f54b40dae0dc97e
anibilthare <44436898+anibilthare@users.noreply.github.com>
2020-05-16 23:44:01 +0530

    Update Scanner.js

--- Scanner.js
+++ Scanner.js
@@ -7,7 +7,9 @@ document.body.addEventListener("click", (e) => {
     let height = window.getComputedStyle(e.target).getPropertyValue("height");
     let width = window.getComputedStyle(e.target).getPropertyValue("width");
     let position = window.getComputedStyle(e.target).getPropertyValue("position");
-    alert(`height: ${height}\nWidth: ${width}\nPosition: ${position}\nFont-size: ${fontSize}\nFont-family: ${fontFamily}`);
+    let fontColor = window.getComputedStyle(e.target).getPropertyValue("color");
+    let backgroundColor = window.getComputedStyle(e.target).getPropertyValue("background-color")
+    alert(`height: ${height}\nWidth: ${width}\nPosition: ${position}\nFont color: ${fontColor}\nBackground-color: ${backgroundColor}\nFont-size: ${fontSize}\nFont-family: ${fontFamily}`);
 })
 document.body.addEventListener("mouseover", (e) => {
     e.target.style.border = 'red dotted';
